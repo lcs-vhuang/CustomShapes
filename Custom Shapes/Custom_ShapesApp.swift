943802cdd610432cb409cf06afd87d2794ebbb0e
Venessa Huang <vhuang@lcs.on.ca>
2026-02-27 19:09:14 -0500

    Initial Commit

---
+++ Custom Shapes/Custom_ShapesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Custom_ShapesApp.swift
+//  Custom Shapes
+//
+//  Created by 黃翊喬 on 2026/2/27.
+//
+
+import SwiftUI
+
+@main
+struct Custom_ShapesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
